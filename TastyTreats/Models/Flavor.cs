789135229b6a0afe956c90470e8997c67414cfe3
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-04-06 19:06:49 -0700

    add flavor class with navigation properties for joinentity and applicationuser

---
+++ TastyTreats/Models/Flavor.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace TastyTreats.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+
+    [Required(ErrorMessage = "Your flavor must have a name")]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+     public List<TreatFlavor> JoinEntities { get; }
+
+     public ApplicationException User { get; set; }
+  }
+}
