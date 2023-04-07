e31c2d3f675af746fe1b85650fd358912288f275
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-04-06 20:32:04 -0700

    WIP: start adding actions to treats controller-index action added for treats index view

---
+++ TastyTreats/Controllers/TreatsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using TastyTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace TastyTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly TastyTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, TastyTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      List<Treat> userTreats = _db.Treats
+                          .Where(entry => entry.User.Id == currentUser.Id)
+                          .ToList();
+      return View(userTreats);
+    }
+
+  }
+}
