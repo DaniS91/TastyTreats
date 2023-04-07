b9c7eb43b6c763dbf3090108d08835fe9a1a0bd6
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-04-06 19:06:03 -0700

    add treat class with navigation properties for joinentity and applicationuser

---
+++ TastyTreats/Models/Treat.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace TastyTreats.Models
+{
+  public class Treat
+  {
+    public int TreatId { get; set; }
+
+    [Required(ErrorMessage = "Your treat must have a name")]
+    public string Name { get; set; }
+
+     public string Description{ get; set; }
+
+     public List<TreatFlavor> JoinEntities { get; }
+
+     public ApplicationUser User { get; set; }
+  }
+}
