e1c3036db117bc72866a2a4dd3b4251aa11d9664
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-04-07 19:27:37 -0700

    add Create actions to Treat Controller

--- TastyTreats/Controllers/TreatsController.cs
+++ TastyTreats/Controllers/TreatsController.cs
@@ -32,5 +32,27 @@ namespace TastyTreats.Controllers
       return View(userTreats);
     }
 
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat)
+    {
+      if(ModelState.IsValid)
+      {
+        return View(treat);
+      }
+      else
+      {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        treat.User = currentUser;
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
   }
 }
